9226a7a69972a6a28a6c313efee5798e9b4a9722
petekgithub <petekshamitbeyli@gmail.com>
2022-07-04 01:05:13 +0300

    fetch-and-user-interaction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(result => setProducts(result))
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Heyyy!</h2>
+      {products.map(product => (
+          <li>{product.category}</li>
+      ))}
     </div>
   );
 }
